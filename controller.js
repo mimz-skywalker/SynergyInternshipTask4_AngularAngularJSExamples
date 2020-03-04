f67932f0009ab417dbfc47f4ccf38487903ba94e
mariyap <mariqpetkova98@abv.bg>
2020-03-04 11:57:39 +0200

    04/03

--- controller.js
+++ controller.js
@@ -1,17 +1,9 @@
-angular.module('mycontroller', [])
-.controller('ExampleController', function ExampleController(){
-    this.username = 'maria';
-    this.password = 'mariapass123';
-
-    this.login = function Login(username, password){
-        if (this.username != username || this.password != password)
-        {
-            window.alert('Login unsuccessful');
-        }
-        else 
-        {
-            window.alert('Login successful')
-        }
-    }
-
-})
+angular.module('controller', [])
+.controller('ExampleController', ['$username', "$pass", function( $username, $pass) {
+  $username = 'maria';
+  $pass = 'maria123';
+  function Login($username, $pass){
+     
+    $window.alert('alvnl/adfn');
+  };
+}]);
